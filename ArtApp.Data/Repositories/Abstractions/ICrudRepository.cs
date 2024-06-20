cc7c2b2f0b6d47f050e74040cbf53d24680452fd
peshopbs2 <peshopbs2@gmail.com>
2024-06-19 21:04:43 -0800

    add repository pattern implmentation

---
+++ ArtApp.Data/Repositories/Abstractions/ICrudRepository.cs
@@ -0,0 +1,21 @@
+﻿using ArtApp.Data.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtApp.Data.Repositories.Abstractions
+{
+    public interface ICrudRepository<T>
+        where T : BaseEntity
+    {
+        public Task AddAsync(T entity);
+        public Task UpdateAsync(T entity);
+        public Task DeleteByIdAsync(int id);
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+        public Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+    }
+}
