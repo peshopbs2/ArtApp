41d639626d2d94f6ce79b2d142545d573a98197e
peshopbs2 <peshopbs2@gmail.com>
2024-06-19 22:00:36 -0800

    add dbcontext configuration for scaffolding

--- ArtApp.Data/ApplicationDbContext.cs
+++ ArtApp.Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using ArtApp.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace ArtApp.Web.Data
 {
@@ -12,5 +14,21 @@ namespace ArtApp.Web.Data
         }
 
         public DbSet<Artist> Artists { get; set; }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDbContext(builder.Options);
+        }
     }
 }
