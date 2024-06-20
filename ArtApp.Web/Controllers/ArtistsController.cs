323471cc390965ed8273f8acd73d44e2bf4713db
peshopbs2 <peshopbs2@gmail.com>
2024-06-19 22:01:11 -0800

    add artist controller

---
+++ ArtApp.Web/Controllers/ArtistsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ArtApp.Data.Entities;
+using ArtApp.Web.Data;
+using ArtApp.Services.Abstractions;
+using ArtApp.Services.DTOs;
+
+namespace ArtApp.Web.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly IArtistService _service;
+
+        public ArtistsController(IArtistService service)
+        {
+            _service = service;
+        }
+
+        // GET: Artists
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.GetAllAsync());
+        }
+
+        // GET: Artists/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _service.GetByIdAsync(id.Value);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // GET: Artists/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ArtistDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.AddAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: Artists/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _service.GetByIdAsync(id.Value);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ArtistDTO model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.UpdateAsync(model);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtistExists(model.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: Artists/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artist = await _service.GetByIdAsync(id.Value);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return View(artist);
+        }
+
+        // POST: Artists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _service.DeleteByIdAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtistExists(int id)
+        {
+            return _service.GetByIdAsync(id) != null;
+        }
+    }
+}
